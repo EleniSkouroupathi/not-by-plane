fa3ec0d42b1b6eddded1f2a76ba1e92c12813172
Ezzy Elliott <ezzy.elliott@gmail.com>
2015-12-08 12:05:14 +0000

    closes #3 by correcting spelling

--- app/noPlaneController.js
+++ app/noPlaneController.js
@@ -6,8 +6,8 @@ notplaneapp.controller('noPlaneController',['$resource',function($resource) {
 
   var API_key = 'INQs3X0RjI9ZHCCMZM1pLyRx1nsNz6TC'; //EE
 
-  self.getFlightFromAirport = function(startLoc,endLoc,startDate,endDate) {
-    searchResource = $resource(lowFareSearch+'?'+'apikey='+API_key+'&origin='+startLoc+'&destination='+endLoc+'&departure_date='+startDate+'&return_date=');
+  self.getFlightFromLoc = function(startLoc,endLoc,startDate,endDate) {
+    searchResource = $resource(lowFareSearch+'?'+'apikey='+API_key+'&origin='+startLoc+'&destination='+endLoc+'&departure_date='+startDate+'&return_date='+endDate);
     searchResource.get().$promise.then(function(response) {
       self.response = response;
     });
